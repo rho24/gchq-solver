c0c5f67d6a4517be02b2f3a797d5e301ffc074d4
Richard Oliver <richard@rholiver.co.uk>
2016-01-22 23:48:08 +0000

    Refactored improving code

--- gchq-solver.fs
+++ gchq-solver.fs
@@ -8,38 +8,106 @@ open solver
 open Fuchu
 open PuzzleTests
 
+type Axis = 
+  | Row
+  | Column
+
+type AxisChange = 
+  { Orientation : Axis
+    Index : int
+    NewAxisState : CellResult list }
+
+type PuzzleState = 
+  { RowTests : int list list
+    ColumnTests : int list list
+    Cells : CellResult [,]
+    AxisChanges : AxisChange list }
+
+let tryImproveSolution (state : PuzzleState) (possiblesCache : Map<int * int list, CellResult list list>) 
+    (orientation : Axis) (index : int) = 
+  match orientation with
+  | Row -> 
+    let currentRowState = row index state.Cells
+    let test = state.RowTests.[index]
+    let possibles = possiblesCache.Item(currentRowState.Length, test)
+    let newRowState = trySolveAxis currentRowState test possibles
+    match currentRowState = newRowState with
+    | true -> state
+    | false -> 
+      let change = 
+        { Orientation = orientation
+          Index = index
+          NewAxisState = newRowState }
+      { state with Cells = 
+                     rows state.Cells
+                     |> List.mapi (fun i r -> 
+                          if i <> index then r
+                          else newRowState)
+                     |> array2D
+                   AxisChanges = change :: state.AxisChanges }
+  | Column -> 
+    let currentColumnState = column index state.Cells
+    let test = state.ColumnTests.[index]
+    let possibles = possiblesCache.Item(currentColumnState.Length, test)
+    let newColumnState = trySolveAxis currentColumnState test possibles
+    match currentColumnState = newColumnState with
+    | true -> state
+    | false -> 
+      let change = 
+        { Orientation = orientation
+          Index = index
+          NewAxisState = newColumnState }
+      { state with Cells = 
+                     columns state.Cells
+                     |> List.mapi (fun i r -> 
+                          if i <> index then r
+                          else newColumnState)
+                     |> array2D
+                     |> rotate
+                   AxisChanges = change :: state.AxisChanges }
+
 let guessPuzzle = 
   fun () -> 
-    let origPosition = (Console.CursorLeft, Console.CursorTop)
     let start = convertPatternToResult Puzzle.initialGrid
     let width = Array2D.length2 start
     let height = Array2D.length1 start
-    let possiblesForRows = Puzzle.rowTests |> List.map (fun x -> getPossibles width x)
+    let possiblesForRows = Puzzle.rowTests |> List.mapi (fun i x -> ((width, x), getPossibles width x))
     printfn "Calculated row possibles"
-    let possiblesForColumns = Puzzle.colTests |> List.map (fun x -> getPossibles height x)
+    let possiblesForColumns = Puzzle.colTests |> List.mapi (fun i x -> ((height, x), getPossibles height x))
     printfn "Calculated column possibles"
-    let mutable guess = start
-    seq { 0..100 }
-    |> Seq.tryFind (fun i -> 
-         guess <- rows guess
-                  |> List.map3 trySolveAxis
-                  <| Puzzle.rowTests
-                  <| possiblesForRows
-                  |> array2D
-         Console.SetCursorPosition origPosition
-         printResultGrid guess Puzzle.rowTests Puzzle.colTests |> Console.WriteLine
-         guess <- columns guess
-                  |> List.map3 trySolveAxis
-                  <| Puzzle.colTests
-                  <| possiblesForColumns
-                  |> array2D
-                  |> rotate
-         Console.SetCursorPosition origPosition
-         printResultGrid guess Puzzle.rowTests Puzzle.colTests |> Console.WriteLine
-         let finished = rows guess |> List.forall (fun r -> List.forall (fun c -> c <> U) r)
-         if finished then printfn "\nFinished after %i" i
-         finished)
-    |> ignore
+    let possiblesCache = List.append possiblesForRows possiblesForColumns |> Map.ofList
+    let origPosition = (Console.CursorLeft, Console.CursorTop)
+    
+    let mutable currentState = 
+      { RowTests = Puzzle.rowTests
+        ColumnTests = Puzzle.colTests
+        Cells = start
+        AxisChanges = [] }
+    
+    let finishedIndex = 
+      seq { 0..1000 } |> Seq.tryFind (fun i -> 
+                           let testIndex = i % (width + height)
+                           
+                           let orientation = 
+                             if testIndex < height then Row
+                             else Column
+                           
+                           let index = 
+                             match orientation with
+                             | Row -> testIndex
+                             | Column -> testIndex - height
+                           
+                           currentState <- tryImproveSolution currentState possiblesCache orientation index
+                           Console.SetCursorPosition origPosition
+                           printfn "%A\t%d " orientation index
+                           printResultGrid currentState.Cells Puzzle.rowTests Puzzle.colTests |> Console.WriteLine
+                           let finished = 
+                             rows currentState.Cells |> List.forall (fun r -> List.forall (fun c -> c <> U) r)
+                           finished)
+    
+    match finishedIndex with
+    | None -> printfn "Failed to complete" |> ignore
+    | Some i -> printfn "Complete on index %i" i |> ignore
 
 [<EntryPoint>]
 let main argv = 
